6bf3e7866579f224737d127a4f9ee81f8e1d5343
mbshusta <60150773+mbshusta@users.noreply.github.com>
2020-12-13 15:39:42 -0500

    Add files via upload

---
+++ Lab 6/source files/leddec16.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/11/2020 04:33:29 PM
+// Design Name: 
+// Module Name: leddec16
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module leddec16(
+
+    );
+endmodule
